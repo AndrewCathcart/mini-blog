df9d1250313792a4cce5ad35862fb75f5360f5cc
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-08-09 21:56:01 +0100

    removed .http files, just using postman, comments/index.js file done

--- comments/index.js
+++ comments/index.js
@@ -5,11 +5,23 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 app.use(bodyParser.json());
 
-const comments = {};
+const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {});
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.post('/posts/:id/comments', (req, res) => {});
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = uuidv4();
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
 
 app.listen(4001, () => {
   console.log('Comments service listening on port 4001');
